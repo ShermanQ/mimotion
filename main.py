a8c3944ce8156e96bde94918b1355cef983d47ff
Toogle <25702506+ShermanQ@users.noreply.github.com>
2022-04-20 15:56:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ step1 = ""
 # 开启根据地区天气情况降低步数（默认关闭）
 open_get_weather = sys.argv[12]
 # 设置获取天气的地区（上面开启后必填）如：area = "宁波"
-area = sys.argv[13]
+area = 'NO'
 
 # 以下如果看不懂直接默认就行只需改上面
 
@@ -44,9 +44,9 @@ time_list = [8, 10, 13, 15, 17, 19, 21]
 set_push = [True, True, True, True, True, True, True]
 
 # 最小步数（如果只需要刷步的次数少于7次就将该次数以后的步数全都改成0，如：time_list[3]: 0，表示第五次开始不运行或者直接云函数触发里面不在该时间调用均可（建议用后者））
-min_dict = {time_list[0]: 6000, time_list[1]: 10000, time_list[2]: 20000, time_list[3]: 30000, time_list[4]: 40000, time_list[5]: 50000, time_list[6]: 60000}
+min_dict = {time_list[0]: 6000, time_list[1]: 10000, time_list[2]: 0, time_list[3]: 0, time_list[4]: 0, time_list[5]: 0, time_list[6]: 0}
 # 最大步数（例如现在设置意思是在8点（你设置的第一个时间点默认8）运行会在1500到2999中随机生成一个数提交（开启气候降低步数会乘系数K）10点3000~4999。。。以此类推，步数范围建议看懂了再改，没看懂直接默认就好）
-max_dict = {time_list[0]: 9999, time_list[1]: 19999, time_list[2]: 29999, time_list[3]: 39999, time_list[4]: 49999, time_list[5]: 59999, time_list[6]: 69999}
+max_dict = {time_list[0]: 9999, time_list[1]: 19999, time_list[2]: 0, time_list[3]: 0, time_list[4]: 0, time_list[5]: 0, time_list[6]: 0}
 # 设置结束
 #now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 # 北京时间
